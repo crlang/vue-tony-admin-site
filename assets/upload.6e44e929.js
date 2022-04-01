66516e256d96b317c177c293f3114ab634e1ffe6
crlang <crlang@163.com>
2022-04-01 16:26:40 +0800

    Upgrade Demo

--- assets/upload.6e44e929.js
+++
@@ -1,1 +0,0 @@
-import{d as e,ar as r}from"./index.ec0491db.js";const{uploadUrl:u=""}=e();function d(o,t){return r.uploadFile({url:u,onUploadProgress:t},o)}export{d as u};
---
+++ assets/upload.a951f1a6.js
@@ -0,0 +1,1 @@
+import{d as e,ar as r}from"./index.16f9732e.js";const{uploadUrl:u=""}=e();function l(o,t){return r.uploadFile({url:u,onUploadProgress:t},o)}export{l as u};
